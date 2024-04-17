c31767f64537f20270115dc8f40df514de1490f6
MUHAMMAD ASIF <162907407+Muhammadasif1986@users.noreply.github.com>
2024-04-17 07:52:03 -0700

    first commit
    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const Anwser = await inquirer.prompt([
+    {
+        name: 'sentence',
+        type: 'input',
+        message: 'Enter your paragraph to count the words'
+    }
+]);
+// .trim() using for delete white spaces
+// .split() using for converting data paragraph/string to array like("i", "am", "going", "to", "home").
+// .split(" ") to space in between (" ") to segregate each words if are removed space("") that each alphabet are segregate
+const words = Anwser.sentence.trim().split(" ");
+console.log(words);
+// .length measure length of array
+console.log(`your paragraph to count ${words.length} words`);
